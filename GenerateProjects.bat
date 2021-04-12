370c196e5dd77cc314f2bdb87ae6fdcc0133a60f
杨玉印 <504097978@qq.com>
2021-04-12 23:49:54 +0800

    Premake工具

---
+++ GenerateProjects.bat
@@ -0,0 +1,4 @@
+@chcp 65001
+@title 生结解决方案...
+call vendor\bin\premake\premake5.exe vs2019
+pause
---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "MadSword"
+    architecture "x86_64"
+
+    configurations{
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+--临时变量 定义 输出目录
+--详细的所有支持的tokens
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MadSword" --项目名称
+    location "MadSword" --相对路径
+    kind "SharedLib"    --表明该项目是dll动态库
+    language "c++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")--输出目录
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")--中间临时文件
+
+    files--该项目的文件
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs --包含目录
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows" --windows平台配置
+        cppdialect "c++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines --预编译宏
+        {
+            "MS_BUILD_DLL",
+            "MS_PLATFORM_WINDOWS",
+            "_WINDLL",
+            "_UNICODE",
+            "UNICODE",
+        }
+
+        postbuildcommands --build后的自定义命令
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox") --拷贝引擎库文件
+        }
+
+    filter "configurations:Debug"
+        defines "MS_DEBUG"
+        runtime "Debug"
+        symbols "on"
+    
+    filter "configurations:Release"
+        defines "MS_RELEASE"
+        runtime "Release"
+        optimize "on"
+
+project "Sandbox"
+        location "Sandbox"
+        kind "ConsoleApp"
+        language "c++"
+
+        targetdir("bin/" .. outputdir .. "/%{prj.name}")--输出目录
+        objdir("bin-int/" .. outputdir .. "/%{prj.name}")--中间临时文件
+
+        files--该项目的文件
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+
+        includedirs --包含目录
+        {
+            "MadSword/vendor/spdlog/include",
+            "MadSword/src"
+        }
+
+        links
+        {
+            "MadSword"
+        }
+        
+        filter "system:windows" --windows平台配置
+            cppdialect "c++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines --预编译宏
+            {
+                "MS_PLATFORM_WINDOWS",
+                "_UNICODE",
+                "UNICODE",
+            }
+
+        filter "configurations:Debug"
+            defines "MS_DEBUG"
+            runtime "Debug"
+            symbols "on"
+        
+        filter "configurations:Release"
+            defines "MS_RELEASE"
+            runtime "Release"
+            optimize "on"
+        
+        filter "configurations:Dist"
+            defines "MS_DIST"
+            runtime "Release"
+            optimize "on"
+        
